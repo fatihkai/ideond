7a06572b28d35f9433d3e864486dba7f6b6fbb7e
fatihkai <fatihreyhan104@gmail.com>
2026-05-13 22:17:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,20 @@ const demoUsers = [
     email: "mert@ideond.local",
     password: "demo123",
     bio: "Veri görselleştirme, okunabilir grafikler ve karar destek panoları üzerine çalışıyor."
-  }
+  },
+  
+  {
+   id:"user-hasan",
+    name: "Hasan Çoruk",
+    field: "Akademisyen",
+    email: "beun@ideond.local",
+    password: "demo123"
+
+    {
+    
+    
+     
+      
 ];
 
 const demoPosts = [
